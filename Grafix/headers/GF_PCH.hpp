1653b004b8da78775dfd47e1e0b35baef61d5325
s1922254 <t.mansfelt@student.utwente.nl>
2021-02-19 18:37:08 +0100

    Added precompiled headers

---
+++ Grafix/headers/GF_PCH.hpp
@@ -0,0 +1,17 @@
+
+#ifndef GRAFIX_PCH_H
+#define GRAFIX_PCH_H
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#endif
